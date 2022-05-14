715f04789ecafa232105ecd0078a4e1b604fe824
Nayru <nanualexandraramirez@gmail.com>
2022-05-14 18:54:13 -0500

    adding admin configuration

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Categorias, Establecimientos, Contacto, Reserva
+
+from .forms import EstablecimientosForm
 
 # Register your models here.
+
+
+class EstablecimientosAdmin(admin.ModelAdmin):
+
+    list_display = ['nombre', 'ciudad','departamento', 'categoria', 'parqueadero', 'tipo']
+    search_fields = ['nombre', 'categoria', 'ciudad', 'departamento', 'parqueadero', 'tipo']
+    list_filter = ['categoria', 'ciudad', 'departamento', 'tipo']
+    list_per_page = 10
+
+
+admin.site.register(Categorias)
+admin.site.register(Establecimientos, EstablecimientosAdmin)
+admin.site.register(Contacto)
+admin.site.register(Reserva)
+
