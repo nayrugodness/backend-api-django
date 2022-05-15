4b49afba1ea783547344011cbaac1ae98352b7e8
Nayru <nanualexandraramirez@gmail.com>
2022-05-14 20:51:16 -0500

    need to solve the module backend.urls import

--- hoisu/urls.py
+++ hoisu/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from importlib import import_module
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+from hoisu import backend
 
+urlconf_module = import_module(backend)
+
+urlpatterns = [
+    path('', include('backend.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
