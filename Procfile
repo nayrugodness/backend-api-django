465a4147b3fdecd7d62dd55b3e7573e2a803c350
Nayru <nanualexandraramirez@gmail.com>
2022-05-16 22:20:18 -0500

    adding more settings configurations

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web : gunicorn hoisu.wsgi
+web : gunicorn hoisu.wsgi: application --log-file -
 release: python manage.py makemigrations --noinput
 release: python manage.py collectstatic --noinput
 release: python manage.py migrate --noinput
--- hoisu/settings.py
+++ hoisu/settings.py
@@ -14,7 +14,7 @@ import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abs.path(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'hoisu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
